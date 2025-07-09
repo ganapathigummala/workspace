b4219f166edbdd4a04dab1771e4221a4f5c73c0e
GanapathiGummala <ganapathi:@kanilebettu.com>
2025-07-09 23:32:12 +0530

    feat : added boolean flag

--- app/src/main/java/com/gana/workspace/MainActivity.kt
+++ app/src/main/java/com/gana/workspace/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gana.workspace
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,8 +11,10 @@ import com.gana.workspace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var activityMainBinding: ActivityMainBinding
+    val flag:Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        Log.d("dfsfs","$flag")
     }
 }
